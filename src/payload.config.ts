75a925b5b2509f27c9f4aa36f04cd30daf0337bb
viilasa <viilasacontact@gmail.com>
2025-04-21 19:06:07 +0530

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,7 +7,7 @@ import sharp from 'sharp' // sharp-import remains
 
 // ===== ADD CLOUD STORAGE IMPORTS =====
 import cloudStoragePlugin from '@payloadcms/plugin-cloud-storage'; // <-- Use default import
-import { s3Adapter } from '@payloadcms/storage-s3';
+import  s3Adapter  from '@payloadcms/storage-s3';
 // ======================================
 
 // Your Collections and Globals
